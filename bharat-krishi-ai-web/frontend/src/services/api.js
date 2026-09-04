3bafe1caf3a02b2d57bd2d85560996e48717871d
navadeep914 <narranavadeepreddy@gmail.com>
2026-09-04 17:05:25 +0530

    Update api.js

--- bharat-krishi-ai-web/frontend/src/services/api.js
+++ bharat-krishi-ai-web/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://bharat-krishi-ai-backend.onrender.com/api'
+  baseURL: 'https://bharat-krishi-ai.onrender.com/api'
 })
 
 export const cropAPI = {
